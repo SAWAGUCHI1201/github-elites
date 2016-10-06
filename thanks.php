ef936a0243897af1f118f16537683777dfee0cbd
sawaguchi <sawaguchi1201@gmail.com>
2016-10-06 22:10:01 +0000

    add-github-elites

---
+++ thanks.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<title>送信完了</title>
+<meta charset="utf-8">
+<style>
+section {
+  margin-top:150px;
+  text-align:center;
+}
+
+h1 {
+  text-align:center;
+  font-size:15px;
+  font-weight:bold;
+  padding:0;
+  margin:0;
+}
+
+
+a {
+  text-decoration:none;
+  color:#f00;
+  font-size:10px;
+  font-weight:bold;
+  text-align:center;
+  border-bottom:2px solid #f00;
+}
+
+</style>
+</head>
+<body>
+  <section>
+    <h1>お問い合わせを承りました。</h1>
+    <a href="index.php">お問い合わせTOPに戻る</a>
+  </section>
+</body>
+</html>
