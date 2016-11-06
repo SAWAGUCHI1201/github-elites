27340ef95ca32c74fd1878d2fbfa46eb8551e9d2
sawaguchi <sawaguchi1201@gmail.com>
2016-11-06 00:21:58 +0000

    何故か配列を取得できない件

--- Javascript/task/search_form_js/inquiry.php
+++ Javascript/task/search_form_js/inquiry.php
@@ -2,16 +2,93 @@
 // 都道府県コード => 都道府県名
 // 関東地方のみ
 $prefectures = array(
-  "ibaragi"  => "茨城県",
-  "tochigi"  => "栃木県",
-  "gunma" => "群馬県",
-  "saitama" => "埼玉県",
-  "chiba" => "千葉県",
-  "tokyo" => "東京都",
-  "kanagawa" => "神奈川県",
+  8  => "茨城県",
+  9  => "栃木県",
+  10 => "群馬県",
+  11 => "埼玉県",
+  12 => "千葉県",
+  13 => "東京都",
+  14 => "神奈川県",
+);
+$places = array(
+
+  // 栃木県の観光スポット-------------------------------------------------------------------------------
+  9 => array(
+    array(
+      'name'   => '西洋の邸宅',
+      'detail' => '西洋の邸宅はとても良い所です。西洋の邸宅はとても良い所です。
+                   西洋の邸宅はとても良い所です。西洋の邸宅はとても良い所です。
+                   西洋の邸宅はとても良い所です。西洋の邸宅はとても良い所です。
+                   西洋の邸宅はとても良い所です。西洋の邸宅はとても良い所です。',
+      'image'  => 'place_4.jpg',
+    ),
+  ),
+
+  // 群馬県の観光スポット-------------------------------------------------------------------------------
+  10 => array(
+    array(
+      'name'   => '赤い門',
+      'detail' => '赤い門はとても良い所です。赤い門はとても良い所です。
+                   赤い門はとても良い所です。赤い門はとても良い所です。
+                   赤い門はとても良い所です。赤い門はとても良い所です。
+                   赤い門はとても良い所です。赤い門はとても良い所です。',
+      'image'  => 'place_5.jpg',
+    ),
+  ),
+
+  // 東京都の観光スポット-------------------------------------------------------------------------------
+  13 => array(
+    array(
+      'name'   => '緑の階段',
+      'detail' => '緑の階段はとても良い所です。緑の階段はとても良い所です。
+                   緑の階段はとても良い所です。緑の階段はとても良い所です。
+                   緑の階段はとても良い所です。緑の階段はとても良い所です。
+                   緑の階段はとても良い所です。緑の階段はとても良い所です。',
+      'image'  => 'place_1.jpg',
+    ),
+    array(
+      'name'   => '雷門',
+      'detail' => '雷門はとても良い所です。雷門はとても良い所です。雷門はとても良い所です。
+                   雷門はとても良い所です。雷門はとても良い所です。雷門はとても良い所です。
+                   雷門はとても良い所です。雷門はとても良い所です。',
+      'image'  => 'place_6.jpg',
+    ),
+    array(
+      'name'   => '東京タワー',
+      'detail' => '東京タワーはとても良い所です。東京タワーはとても良い所です。
+                   東京タワーはとても良い所です。東京タワーはとても良い所です。
+                   東京タワーはとても良い所です。東京タワーはとても良い所です。
+                   東京タワーはとても良い所です。東京タワーはとても良い所です。',
+      'image'  => 'place_7.jpg',
+    ),
+  ),
+
+  // 神奈川県の観光スポット-------------------------------------------------------------------------------
+  14 => array(
+    array(
+      'name'   => '日本の城',
+      'detail' => '日本の城はとても良い所です。日本の城はとても良い所です。
+                   日本の城はとても良い所です。日本の城はとても良い所です。
+                   日本の城はとても良い所です。日本の城はとても良い所です。
+                   日本の城はとても良い所です。日本の城はとても良い所です。',
+      'image'  => 'place_2.jpg',
+    ),
+    array(
+      'name'   => '旅館の部屋',
+      'detail' => '旅館の部屋はとても良い所です。旅館の部屋はとても良い所です。
+                   旅館の部屋はとても良い所です。旅館の部屋はとても良い所です。
+                   旅館の部屋はとても良い所です。旅館の部屋はとても良い所です。
+                   旅館の部屋はとても良い所です。旅館の部屋はとても良い所です。',
+      'image'  => 'place_3.jpg',
+    ),
+  ),
 );
 
-$articles = 0;
+// $articles = 0;
+// if (isset($_POST['pref'])) {
+//   $Pref = $_POST['pref'];
+//   $articles = count($places[$Pref]);
+// }
 ?>
 <!DOCTYPE html>
 <html>
@@ -24,14 +101,14 @@ $articles = 0;
   h1 {margin: 0 0 30px 0;padding: 20px 30px;border-bottom: 1px solid #ccc;background-color: #f8f8f8;}
   footer {text-align:center;}
   .search-result {margin: 10px 0;}
-  .media {display:none;}
+  /*.media {display:none;}
   </style>
 </head>
 <body>
   <h1>関東地方の観光スポット検索(js)</h1>
   <div class="container">
 
-    <form class="form-inline" action="inquiry.php" method="post">
+    <form class="form-inline">
       <div class="form-group">
         <select name="pref" class="form-control">
           <option value="reset">選択してください</option>
@@ -41,107 +118,25 @@ $articles = 0;
         </select>
       </div>
     </form>
+
     <button id="remove">リセット</button>
+
     <p class="search-result">
-      <span>あとでやる</span>
+      <span>件数の表示はあとでやる</span>
     </p>
 
-
-    <div class="media tochigi">
-      <div class="media-left">
-            <img src="place_4.jpg">
-      </div>
-      <div class="media-body">
-        <h4>西洋の邸宅</h4>
-        <p>西洋の邸宅はとても良い所です。西洋の邸宅はとても良い所です。
-           西洋の邸宅はとても良い所です。西洋の邸宅はとても良い所です。
-           西洋の邸宅はとても良い所です。西洋の邸宅はとても良い所です。
-           西洋の邸宅はとても良い所です。西洋の邸宅はとても良い所です。
-        </p>
-      </div>
-    </div>
-
-      <div class="media gunma">
+    <?php foreach($places as $in_place => $place): ?>
+      <div class="media">
         <div class="media-left">
-              <img src="place_5.jpg">
+              <img src="<?php echo $place['image'] ?>">
         </div>
         <div class="media-body">
-          <h4>赤い門</h4>
-          <p>赤い門はとても良い所です。赤い門はとても良い所です。
-             赤い門はとても良い所です。赤い門はとても良い所です。
-             赤い門はとても良い所です。赤い門はとても良い所です。
-             赤い門はとても良い所です。赤い門はとても良い所です。
-          </p>
+          <?php echo '<h4>' . $place["name"] . '</h4>'; ?>
+          <?php echo '<p>' . $place["detail"] . '</p>'; ?>
         </div>
       </div>
-
-      <div class="media tokyo">
-        <div class="media-left">
-              <img src="place_1.jpg">
-        </div>
-        <div class="media-body">
-          <h4>緑の階段</h4>
-          <p>緑の階段はとても良い所です。緑の階段はとても良い所です。
-             緑の階段はとても良い所です。緑の階段はとても良い所です。
-             緑の階段はとても良い所です。緑の階段はとても良い所です。
-             緑の階段はとても良い所です。緑の階段はとても良い所です。
-          </p>
-        </div>
-      </div>
-      <div class="media tokyo">
-        <div class="media-left">
-              <img src="place_6.jpg">
-        </div>
-        <div class="media-body">
-          <h4>雷門</h4>
-          <p>雷門はとても良い所です。雷門はとても良い所です。雷門はとても良い所です。
-             雷門はとても良い所です。雷門はとても良い所です。雷門はとても良い所です。
-             雷門はとても良い所です。雷門はとても良い所です。
-          </p>
-        </div>
-      </div>
-      <div class="media tokyo">
-        <div class="media-left">
-              <img src="place_7.jpg">
-        </div>
-        <div class="media-body">
-          <h4>東京タワー</h4>
-          <p>東京タワーはとても良い所です。東京タワーはとても良い所です。
-             東京タワーはとても良い所です。東京タワーはとても良い所です。
-             東京タワーはとても良い所です。東京タワーはとても良い所です。
-             東京タワーはとても良い所です。東京タワーはとても良い所です。
-          </p>
-        </div>
-      </div>
-
-
-      <div class="media kanagawa">
-        <div class="media-left">
-              <img src="place_2.jpg">
-        </div>
-        <div class="media-body">
-          <h4>日本の城</h4>
-          <p>日本の城はとても良い所です。日本の城はとても良い所です。
-             日本の城はとても良い所です。日本の城はとても良い所です。
-             日本の城はとても良い所です。日本の城はとても良い所です。
-             日本の城はとても良い所です。日本の城はとても良い所です。
-          </p>
-        </div>
-      </div>
-      <div class="media kanagawa">
-        <div class="media-left">
-              <img src="place_3.jpg">
-        </div>
-        <div class="media-body">
-          <h4>旅館の部屋</h4>
-          <p>旅館の部屋はとても良い所です。旅館の部屋はとても良い所です。
-             旅館の部屋はとても良い所です。旅館の部屋はとても良い所です。
-             旅館の部屋はとても良い所です。旅館の部屋はとても良い所です。
-             旅館の部屋はとても良い所です。旅館の部屋はとても良い所です。
-          </p>
-        </div>
-      </div>
-
+    <?php endforeach ?>
+    <?php var_dump($place); ?>
 
   </div>
   <hr>
@@ -159,7 +154,7 @@ $articles = 0;
       var setOption = $("option:selected").val();//選択されたoptionのvalue取得
       var showPref = $(".media."+setOption).show();//取得したvalに該当するクラスを表示
       // $("div."+setOption:not("div")).hide();
-      $(".media."+setOption).not(".media").hide(".media");//選択されたoption以外を非表示
+      //$(".media."+setOption).not(".media").hide(".media");//選択されたoption以外を非表示
     });
   });
   </script>
