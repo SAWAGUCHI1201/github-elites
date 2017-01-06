7b5e72d9d5abdc29f50d6db9eeb86b61b0660610
sawaguchi <sawaguchi1201@gmail.com>
2017-01-07 03:54:40 +0900

    newDirectory

--- objectThinking/task/index.php
+++ objectThinking/task/index.php
@@ -10,40 +10,31 @@ create table members (
 */
 class TableBase//----------------------------------------------------------
 {
-  public $data;//配列
+  public $data;
   public $tableName;//テーブル
 
   public function set($get_data) {
     $this->data = $get_data;//定義した空のメンバ変数に代入する
   }
 
-  public function insert() {
-    function connectDatabase()
-    {
-     try
-        {
-          $dbh = new PDO('mysql:host=localhost;dbname=obj_task;charset=utf8', 'userName', '0000', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-          return $dbh;//戻り値設定
-        }
-        catch (PDOException $e)
-        {
-          echo $e->getMessage();
-          exit;
-        }
-    }
-    $get_data = $this->data;//bindParam用で代入
-    $sql = 'insert into ' . $tableName . '(name,age,email,created_at) values(:name,:age,:email,now())';
-    $stmt = $dbh->prepare($sql);
-    $stmt->bindParam(":name", $get_data['name']);
-    $stmt->bindParam(":age", $get_data['age']);
-    $stmt->bindParam(":email", $get_data['email']);
-    $stmt->execute();
+  //理想はinsert()入れる
 
+  public function connectDatabase()
+  {
+   try
+      {
+        $dbh = new PDO('mysql:host=localhost;dbname=obj_task;charset=utf8', 'userName', '0000', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        return $dbh;//戻り値設定
+      }
+      catch (PDOException $e)
+      {
+        echo $e->getMessage();
+        exit;
+      }
   }
 
   public function delete($result){
-    $dbh = $this->connectDatabase();//$dbhの処理
-
+    $dbh = $this->connectDatabase();
     $sql = 'delete from ' . $this->tableName . ' where id = :id';
     $stmt = $dbh->prepare($sql);
     $stmt->bindParam(':id',$result);
@@ -54,24 +45,48 @@ class TableBase//----------------------------------------------------------
 class ShopItem extends TableBase//---------------------------------------------
 {
   public $tableName = 'shop_items';//オーバーライド
+
+  public function insert() {
+    $dbh = $this->connectDatabase();
+    $get_data = $this->data;//bindParam用で代入
+    $sql = 'insert into' . $this->tableName . '(code,name,price,created_at) values(:code,:name,:price,now())';
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindParam(":code", $get_data['code']);
+    $stmt->bindParam(":name", $get_data['name']);
+    $stmt->bindParam(":price", $get_data['price']);
+    $stmt->execute();
+  }
+
   public function findByCode($fetchCode = null)
   {
     $dbh = $this->connectDatabase();
     $sql = 'select * from members where code = :code';
     $stmt = $dbh->prepare($sql);
-    $stmt->bindParam(':code',$fetchCode['code']);
+    $stmt->bindParam(':code',$fetchCode);
     $stmt->execute();
     $row = $stmt->fetch();
     //fetchAll(全データを配列に変換 全ての結果行を含む配列を返す)
     ///全ての結果行なのでここでは全て(配列)の中の結果行(配列)となる。
-    var_dump($row);
     return $row;
   }
+
 }
 
 class Member extends TableBase//-----------------------------------------------
 {
   public $tableName = 'members';//オーバーライド
+
+  public function insert() {
+    $dbh = $this->connectDatabase();
+    $get_data = $this->data;//bindParam用で代入
+    $sql = 'insert into ' . $this->tableName . '(name,age,email,created_at) values(:name,:age,:email,now())';
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindParam(":name", $get_data['name']);
+    $stmt->bindParam(":age", $get_data['age']);
+    $stmt->bindParam(":email", $get_data['email']);
+    $stmt->execute();
+  }
+
   public function findByEmail($fetchEmail = null)
   {
     $dbh = $this->connectDatabase();
@@ -82,32 +97,33 @@ class Member extends TableBase//-----------------------------------------------
     $row = $stmt->fetch();
     //fetchAll(全データを配列に変換 全ての結果行を含む配列を返す)
     ///全ての結果行なのでここでは全て(配列)の中の結果行(配列)となる。
-    var_dump($row);
     return $row;
   }
 }
 
+
+
 // ------------------------------------------------------------
 $ShopItem = new ShopItem();
 $ShopItem->set(array(
-   'code' => 1,
-   'name' => 'Macbook Air',
+   'code' => '1201',
+   'name' => 'MacbookAir',
    'price' => 148000 ,
    ));
-
+$ShopItem->insert();
 //$result = $ShopItem->delete(1);
-var_dump($ShopItem);
 exit;
 // ------------------------------------------------------------
-$member = new Member();
-$member->set(array(
-   'name' => 'テストネーム1',
-   'age' => 30,
-   'email' => 'test@example.com',
-   ));
-$result = $member->delete(1);
-var_dump($member);
-exit;
+// $member = new Member();
+// $member->set(array(
+//    'name' => 'ぐっちー',
+//    'age' => 30,
+//    'email' => 'sawaguchi2@example.com',//次は違うやつで
+//    ));
+// $member->insert();
+// //$result = $member->delete(1);
+// var_dump($member);
+// exit;
 
 
 
